f620c4cdc782f304143913986a2cd6dbdde4276e
junhuaLiang <javaljh@163.com>
2020-12-22 15:36:13 +0800

    init

---
+++ xcx/xcx-config/src/main/java/com/cqrcb/xcxconfig/XcxConfigApplication.java
@@ -0,0 +1,13 @@
+package com.cqrcb.xcxconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcxConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XcxConfigApplication.class, args);
+    }
+
+}
